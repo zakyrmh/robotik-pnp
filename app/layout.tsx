5f48897a4cbd147aac962b2b66edec04070e3afa
zakyrmh <zaxxyyramadhan@gmail.com>
2025-09-01 15:03:42 +0700

    feat: web analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f3f4f6] dark:bg-[#020d1a]`}>
+        <Analytics/>
         <ThemeProvider defaultTheme="light" attribute="class">
           {children}
         </ThemeProvider>
