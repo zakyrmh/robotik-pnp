939861e2f4713ee1850dbc7d5fa574664f527a57
zakyrmh <zaxxyyramadhan@gmail.com>
2025-09-04 12:38:03 +0700

    feat: install nextjs

--- app/(admin)/account/settings/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AccountSettings() {
-    return (
-        <div>
-            <h1>Account Settings</h1>
-        </div>
-    );
-}
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "mvorotvbainhtppatvnb.supabase.co",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
