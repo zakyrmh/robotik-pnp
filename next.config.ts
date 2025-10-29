3886a0ff77e4336ee4c4c8d52b0a7109ef16db75
zakyrmh <zaxxyyramadhan@gmail.com>
2025-10-29 19:58:01 +0700

    feat: setup nextjs 16

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "mvorotvbainhtppatvnb.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "drive.google.com",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
